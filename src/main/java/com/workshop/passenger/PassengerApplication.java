221aff66b46c12e31262c28cac79ff0d5ce85a81
ToniGFT <aipg@gft.com>
2024-11-11 15:31:04 +0100

    Delete src directory

--- src/main/java/com/workshop/passenger/PassengerApplication.java
+++
@@ -1,7 +0,0 @@
-package com.workshop.passenger;
-
-public class PassengerApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/test/java/com/workshop/passenger/PassengerApplicationTests.java
+++
@@ -1,4 +0,0 @@
-package com.workshop.passenger;
-
-public class PassengerApplicationTests {
-}
